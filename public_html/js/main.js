cf9b35dd7e272eeee76b8d560ccd433be8a4c10e
Paweł <https://github.com/Pawecio/Projekt.git>
2018-06-22 06:54:24 +0200

    zapis

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,1 +1,43 @@
 
+//$( document ).ready(function() {
+//    var container=$(".serv_img");
+//    
+//    console.log(container);
+//    
+// container.mousemove(function() {
+//     console.log("test");
+// container.slideDown( "slow", function() {
+//    // Animation complete.
+//  });
+//});
+//
+//});
+//var h2 = $('h2');
+//console.log(h2);
+//var test = $("#test")
+//console.log(test);
+//
+//test.on(function() {
+//    console.log("huehue");
+//    console.log(test);
+//  $( ".serv_img_1" ).animate({ 
+//        width: "100px",
+//        height: "100px"
+//    }, "slow" );
+//});
+
+//$(".serv_img_1").mouseenter(function() {
+//    $('.serv_img_1').stop().animate({
+//        left: "0"
+//    }, 1000);
+//});
+//
+//$(".serv_img_1").mouseleave(function() {
+//    $('.serv_img_1').stop().animate({
+//        left: "-270"
+//    }, 1000);
+//});
+
+//$( "#test" ).click(function() {
+//  $( "p" ).toggle( "slow" );
+//});
